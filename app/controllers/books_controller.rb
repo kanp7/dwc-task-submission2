78faaea4977ee30f1b5f1f8a1804521ee9bbb8eb
kanp7 <tatsuya715tk@gmail.com>
2020-05-08 23:30:45 +0900

    エラーメッセージ修正、Rspec完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,9 +30,13 @@ class BooksController < ApplicationController
 
   def update
   	book = Book.find(params[:id])
-  	book.update(book_params)
+  	if book.update(book_params)
     flash[:notice] = "Book was successfully updated."
   	redirect_to book_path(book.id)
+    else
+      @book = book
+      render "edit"
+    end
   end
 
   def destroy
